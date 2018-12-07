c634c7f407827f275f46f74cf4ec2e10e3eed615
SianLouisa <sianlouisa@hotmail.com>
2018-12-07 14:31:41 +0000

    updated files for deployment

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,12 @@
-const knex = require('knex');
-
 const ENV = process.env.NODE_ENV || 'development';
 
-const dbConfig = require('../knexfile')[ENV];
+const config = ENV === 'production'
+  ? { client: 'pg', connection: process.env.DATABASE_URL }
+  : require('../knexfile')[ENV];
 
-const connection = knex(dbConfig);
+// const dbConfig = require('../knexfile')[ENV];
 
-module.exports = connection;
+// const connection = knex(dbConfig);
+module.exports = require('knex')(config);
+
+// module.exports = connection;
--- listen.js
+++ listen.js
@@ -1,10 +1,5 @@
 const app = require('./app');
 
-const PORT = 9090;
+const { PORT = 5432 } = process.env;
 
-app.listen(PORT, (err) => {
-  if (err) console.log(err);
-  else {
-    console.log('listening on 9090');
-  }
-});
+app.listen(PORT, err => console.log(`listening on ${PORT}`));
