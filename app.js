a3608ef6dea797ecaadf56e76ae8ef7f080f4814
SianLouisa <sianlouisa@hotmail.com>
2019-01-07 13:59:22 +0000

    Added CORS

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const bodyParser = require('body-parser');
 const apiRouter = require('./routes/api');
 const listEndpoints = require('express-list-endpoints');
 const { handle404, handle422, handle400 } = require('./errors');
+
+app.use(cors());
 
 app.set('view engine', 'ejs');
 
